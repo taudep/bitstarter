bfff3945820ccfd11900aadb5c4c1b681e432268
Todd Prekaski <taudep@gmail.com>
2013-07-09 11:21:29 -0400

    Finished Part 1 of the homework

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var contents = fs.readFileSync('index.html');
+  console.log(contents);
+  response.send(contents.toString());
 });
 
 var port = process.env.PORT || 5000;
